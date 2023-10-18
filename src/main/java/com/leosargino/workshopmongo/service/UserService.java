139fbb30e29792896b3be27786783be55a16ee01
Leonardo Infante <leosargino@gmail.com>
2023-10-18 15:09:04 -0300

    Entity User e Rest, conexão com Banco(repository e service)

---
+++ src/main/java/com/leosargino/workshopmongo/service/UserService.java
@@ -0,0 +1,20 @@
+package com.leosargino.workshopmongo.service;
+
+import com.leosargino.workshopmongo.domain.User;
+import com.leosargino.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+}
